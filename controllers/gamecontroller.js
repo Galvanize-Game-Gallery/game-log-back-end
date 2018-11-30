73b8c7e47677c16ede7a2dad1355d54a9486e69e
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-30 13:11:59 -0800

    Finished BoilerPlate Code

---
+++ controllers/gamecontroller.js
@@ -0,0 +1,4 @@
+const model = require('../models/gamemodel.js')
+const axios = require('axios')
+
+module.exports = {}
---
+++ controllers/usercontroller.js
@@ -0,0 +1,4 @@
+const model = require('../models/usermodel.js')
+const axios = require('axios')
+
+module.exports = {}

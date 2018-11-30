a3745492b4273dc9ff300dbd7b18e716c68c6ff4
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-30 13:17:46 -0800

    Finished setup of boilderplate and npm modules

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ db('courses')
 }).catch(() => {
   console.log(`ERROR! Connection to Game Gallery DB not found!`)
 }).finally(db.destroy)
+
+//This File is for Testing the database connection! 
+//To utilize, switch the 'main' key in package.json from 'index.js' to 'main.js'

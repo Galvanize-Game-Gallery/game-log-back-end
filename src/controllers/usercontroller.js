f6013e3169421b1e14f0cad893b2fd289e825e95
Andrew Lincoln <andrewjlincoln@gmail.com>
2018-12-04 11:40:52 -0800

    conflicts fixed

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -25,8 +25,34 @@ function create(req, res, next){
     return res.status(201).send({ data })
   })
   .catch(next)
+};
+
+function addPlatform(req, res, next){
+  const {year_purchased, platform_notes, platform_id, user_id} = req.body;
+  userModel.addPlatformToUser(system, year_purchased, platform_notes, platform_id, user_id).then(function(result){
+    res.status(201).send(result)
+  })
+};
+
+function verifyUserPlatform(req,res,next) {
+  if(!req.params.userId) return next({status: 400, message: 'Bad Request, UserID is required'})
+  if(!req.params.platformId) return next({status: 400, message: 'Bad Request, Platform_ID is required'})
+  userModel.verifyUserPlatform(req.params.userId, req.params.platformId)
+  .then(data => {
+    req.upid = data[0].id
+    return next()
+  })
+  .catch(next)
+}
+
+function addToShelf(req,res,next){
+  userModel.addToShelf(req.upid, req.pgid, req.body)
+  .then(result => {
+    res.status(201).send({result})
+  })
+  .catch(next)
 }
 
 module.exports = {
-  create
+  create, verifyUserPlatform, addToShelf, addPlatform
 }

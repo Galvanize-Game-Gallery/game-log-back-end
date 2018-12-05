dc4052d50a9f7c57067d8e985639694762449aa3
Christopher Kistner <chris.kistner0@gmail.com>
2018-12-05 13:44:52 -0800

    Updated getLibrary model to pull respective console names as well.

--- src/models/gamemodel.js
+++ src/models/gamemodel.js
@@ -17,10 +17,18 @@ function verifyPlatformGames(platformid, gameid){
 
 const getLibrary = function() {
     return db('games')
-    .returning('*')
-    .then(data=> {
-        if(!data) throw {status:404, message: 'Uh  Oh Our Library is Missing!'}
-        return data
+    .then(function(library) {
+        const promises = library.map(game => {
+            return db('platform_games')
+            .join('platforms', 'platforms.igdb_id', 'platform_games.platform_id')
+            .where('platform_games.game_id', game.igdb_id)
+            .then(function(data) {
+                // console.log(data)
+                game.platforms = data.name
+                return {...game, platforms: data.map(ele => ele.name) }
+            })
+        })
+        return Promise.all(promises)
     })
 };
 

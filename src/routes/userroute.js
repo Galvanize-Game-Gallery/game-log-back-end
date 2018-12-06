32283e5d6977a724867c100a8216b42e05f9ab34
Christopher Kistner <chris.kistner0@gmail.com>
2018-12-06 08:26:06 -0800

    Put request for user_games_platform edits is now working

--- src/routes/userroute.js
+++ src/routes/userroute.js
@@ -8,5 +8,6 @@ router.post('/', userController.create)
 router.post('/:userId/platforms', userController.addPlatformToUser)
 router.post('/:userId/platforms/:platformId/games', userController.verifyUserPlatform, igdbController.verifyPlatformGames, userController.addToShelf)
 router.delete('/:userId/platforms/:platformId/games/:gameId', userController.dropFromShelf)
+router.put('/:userId/platforms/:platformId/games/:gameId', userController.editGameOnShelf)
 
 module.exports = router
